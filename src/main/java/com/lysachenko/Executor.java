57d25bf698cf709b0de72a64b71f6c4303fb9a44
RL <lysachenko.roman.1099@gmail.com>
2021-12-29 20:27:32 +0200

    some refactor

--- src/main/java/com/lysachenko/Executor.java
+++ src/main/java/com/lysachenko/Executor.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class Executor {
 
-    public static void run() {
+    public void run() {
 
         List<User> users = new ArrayList<>();
 
@@ -37,9 +37,9 @@ public class Executor {
         showSortUsersLimitThree(users);
 
         List<Integer> integerList = new Random()
-                .ints(15, 1, 100)
-                .boxed()
-                .collect(Collectors.toList());
+            .ints(15, 1, 100)
+            .boxed()
+            .collect(Collectors.toList());
 
         System.out.println("\nSource list of integer:");
         integerList.forEach(integer -> System.out.print(integer + " "));
@@ -54,67 +54,67 @@ public class Executor {
         showValuesIncreasedByTen(integerList);
     }
 
-    private static long getCountOfUsersNotLiveInCity(List<User> users, String city) {
+    private long getCountOfUsersNotLiveInCity(List<User> users, String city) {
         return users.stream()
-                .filter(user -> !user.getCity().equals(city))
-                .count();
+            .filter(user -> !user.getCity().equals(city))
+            .count();
     }
 
-    private static long getAverageAgeOfUsersInCity(List<User> users, String city) {
+    private long getAverageAgeOfUsersInCity(List<User> users, String city) {
         return getSumOfAge(users, city) / getCountOfUsers(users, city);
     }
 
-    private static Integer getMinValue(List<Integer> integerList) {
+    private Integer getMinValue(List<Integer> integerList) {
         return integerList.stream().min(Integer::compare).get();
     }
 
-    private static Integer getMaxValue(List<Integer> integerList) {
+    private Integer getMaxValue(List<Integer> integerList) {
         return integerList.stream().max(Integer::compare).get();
     }
 
-    private static void showValuesIncreasedByTen(List<Integer> integerList) {
+    private void showValuesIncreasedByTen(List<Integer> integerList) {
         integerList.stream()
-                .mapToInt(value -> value + 10)
-                .boxed()
-                .forEach(integer -> System.out.print(integer + " "));
+            .mapToInt(value -> value + 10)
+            .boxed()
+            .forEach(integer -> System.out.print(integer + " "));
     }
 
-    private static void showValueMultipleOfTwo(List<Integer> integerList) {
+    private void showValueMultipleOfTwo(List<Integer> integerList) {
         integerList.stream()
-                .filter(integer -> integer % 2 == 0)
-                .forEach(integer -> System.out.print(integer + " "));
+            .filter(integer -> integer % 2 == 0)
+            .forEach(integer -> System.out.print(integer + " "));
     }
 
-    private static void showSortUsersLimitThree(List<User> users) {
+    private void showSortUsersLimitThree(List<User> users) {
         users.stream()
-                .sorted(Comparator.comparingInt(User::getAge))
-                .limit(3)
-                .forEach(System.out::println);
+            .sorted(Comparator.comparingInt(User::getAge))
+            .limit(3)
+            .forEach(System.out::println);
     }
 
-    private static int getSumOfAge(List<User> users, String city) {
+    private int getSumOfAge(List<User> users, String city) {
         return users.stream()
-                .filter(user1 -> user1.getCity().equals(city))
-                .mapToInt(User::getAge)
-                .sum();
+            .filter(user1 -> user1.getCity().equals(city))
+            .mapToInt(User::getAge)
+            .sum();
     }
 
-    private static long getCountOfUsers(List<User> users, String city) {
+    private long getCountOfUsers(List<User> users, String city) {
         return users.stream()
-                .filter(user1 -> user1.getCity().equals(city))
-                .count();
+            .filter(user1 -> user1.getCity().equals(city))
+            .count();
     }
 
-    private static void showUsersUnderFiftyAndLiveInDnipro(List<User> users, String city) {
+    private void showUsersUnderFiftyAndLiveInDnipro(List<User> users, String city) {
         users.stream()
-                .filter(user -> user.getAge() < 50)
-                .filter(user -> user.getCity().equals(city))
-                .forEach(System.out::println);
+            .filter(user -> user.getAge() < 50)
+            .filter(user -> user.getCity().equals(city))
+            .forEach(System.out::println);
     }
 
-    private static void showUsersOverForty(List<User> users) {
+    private void showUsersOverForty(List<User> users) {
         users.stream()
-                .filter(user -> user.getAge() > 40)
-                .forEach(System.out::println);
+            .filter(user -> user.getAge() > 40)
+            .forEach(System.out::println);
     }
 }
--- src/main/java/com/lysachenko/Main.java
+++ src/main/java/com/lysachenko/Main.java
@@ -3,6 +3,6 @@ package com.lysachenko;
 public class Main {
 
     public static void main(String[] args) {
-        Executor.run();
+        new Executor().run();
     }
 }
